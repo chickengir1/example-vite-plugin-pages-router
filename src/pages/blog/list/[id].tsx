b049923fd24dda2dee2362c45b81a29aad3b7336
Lee gangho <rkdgh1198@gmail.com>
2025-02-11 22:35:20 +0900

    chore : 사용 예시 추가 #1

--- src/pages/blog/list/[id].tsx
+++ src/pages/blog/list/[id].tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 
-const List = () => {
+const BlogPostList = () => {
   const id = 1;
   return (
     <div>
-      <h1>목록</h1>
+      <h1>블로그 포스트 리스트 ID: {id}</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
         <Link to="/blog">블로그 페이지로 이동(blog/index)</Link>
+        <Link to="/blog/list">리스트로 이동(blog/list)</Link>
         <Link to={`/blog/${id}`}>
-          {id}번 포스트로 이동(blog/{id})
+          블로그 {id}번 포스트로 이동(blog/{id})
         </Link>
         <Link to="/">홈페이지로 이동(index)</Link>
       </div>
@@ -16,4 +17,4 @@ const List = () => {
   );
 };
 
-export default List;
+export default BlogPostList;
--- src/pages/blog/list/index.tsx
+++ src/pages/blog/list/index.tsx
@@ -1,13 +1,15 @@
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const BlogPost = () => {
-  const { id } = useParams<{ id: string }>();
+const List = () => {
+  const id = 1;
   return (
     <div>
-      <h1>블로그 포스트 ID: {id}</h1>
+      <h1>블로그 포스트 리스트 전체(목록 페이지)</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
         <Link to="/blog">블로그 페이지로 이동(blog/index)</Link>
-        <Link to="/blog/list">리스트로 이동(blog/list)</Link>
+        <Link to={`/blog/${id}`}>
+          블로그 {id}번 포스트로 이동(blog/{id})
+        </Link>
         <Link to={`/blog/list/${id}`}>
           리스트 1번 포스트로 이동(blog/list/{id})
         </Link>
@@ -17,4 +19,4 @@ const BlogPost = () => {
   );
 };
 
-export default BlogPost;
+export default List;
